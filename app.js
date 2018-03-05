3f00e3d11987f14e366de4156fca3bdf2a0772b9
Barly Vallendito <barlyvallendito@gmail.com>
2018-03-05 22:12:17 +0700

    adding script js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ GAME RULES:
 var scores, aroundScore, activePlayer;
 
 scores = [0,0];
-aroundScore = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none'; //for setting css in JS
 
@@ -34,6 +34,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the round score IF the rolled number was NOT a 1
+    if(dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // document.querySelector('.player-0-panel').classList.remove('active'); //menghapus class 
+        // document.querySelector('.player-1-panel').classList.add('active');  //menambah class
+        
+        document.querySelector('.dice').style.display = 'none';
+    }
+
 });
 
 
