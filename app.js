f0ee1bb5b865c9cc3d84b2a9e6d117c38a99342e
Barly Vallendito <barlyvallendito@gmail.com>
2018-03-05 22:42:25 +0700

    adding function hold

--- app.js
+++ app.js
@@ -39,7 +39,54 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
+
+        nextPlayer();
         //next player
+        // activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        // roundScore = 0;
+
+        // document.getElementById('current-0').textContent = '0';
+        // document.getElementById('current-1').textContent = '0';
+
+        // document.querySelector('.player-0-panel').classList.toggle('active');
+        // document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // // document.querySelector('.player-0-panel').classList.remove('active'); //menghapus class 
+        // // document.querySelector('.player-1-panel').classList.add('active');  //menambah class
+        
+        // document.querySelector('.dice').style.display = 'none';
+    }
+
+});
+
+// function Hold
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
+
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+
+    document.querySelector('#score-' +activePlayer).textContent = scores[activePlayer];
+    document.querySelector('.dice').style.display = 'none';
+
+    // Check if player won the game
+
+    if(scores[activePlayer] >= 20) {
+        document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
+    } else {
+        //nextPlayer
+        nextPlayer();
+    }
+
+    
+});
+
+
+function nextPlayer() {
+        //next player
+
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
 
@@ -53,10 +100,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         // document.querySelector('.player-1-panel').classList.add('active');  //menambah class
         
         document.querySelector('.dice').style.display = 'none';
-    }
-
-});
-
+}
 
 
 
